39cb3b6e93b425922671bf2062afbe204f66537c
Dudeldups <130840918+Dudeldups@users.noreply.github.com>
2024-04-21 16:18:21 +0200

    fixed links

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,14 @@
+import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/FM-dine-restaurant",
+  base: process.env.MODE === "production" ? "/FM-dine-restaurant/" : "/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        booking: resolve(__dirname, "booking/index.html"),
+      },
+    },
+  },
 });
